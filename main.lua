e36a2cfbb0ad0beaf77b942dc250813943b885fb
Bean <finbyg@gmail.com>
2023-03-19 16:08:19 +0000

    new file:   .luarc.json 	new file:   main.lua

---
+++ main.lua
@@ -0,0 +1,9 @@
+----Welcome to the "main.lua" file! Here is where all the magic happens, everything from functions to callbacks are done here.
+--Startup
+local mod = RegisterMod("Commission Template - Character", 1)
+local game = Game()
+local rng = RNG()
+
+--local RECOMMENDED_SHIFT_IDX = 35
+--local seeds = game:GetSeeds()
+--local startSeed = seeds:GetStartSeed()
