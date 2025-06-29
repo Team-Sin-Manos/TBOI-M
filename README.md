e7396e83ea46790881f3848082bc7af9ff5479b5
Gabriel Gerardo Carpio Saez De Nanclares <carpiosaezdenanclares@cip.ifi.lmu.de>
2025-06-29 19:53:25 +0200

    -template changed

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# commission-template-character
-This is a template that I use for beginning my The Binding of Isaac: Repentance character commissions/mods on fiverr!
-Want to start using it yourself? Check the wiki: https://github.com/Finbean-commissions/commission-template-character/wiki
+# isaac-character-template
+ A quickstart to making a character for the Binding of Isaac: Repentance
+
+## Things to note:
+
+## Please look at the Tainted Character to see how to properly apply your starting costumes. Otherwise, they'll disappear with D4 or Mom's Knife 2 Sequence
+
+- All of the .anm2 files in the `content/gfx` folder MUST BE NAMED THIS WAY! You can change the .png files as long as you update the .anm2 files, but the .anm2 files must be named that for it to work.
+- The code applies stat changes, costumes, and the character's creep gimmick. Use it as an example.
+- Make an issue with questions.
--- main.lua
+++ main.lua
@@ -1,316 +1,92 @@
-----Welcome to the "main.lua" file! Here is where all the magic happens, everything from functions to callbacks are dOne_Character.
---Startup
-local mod = RegisterMod("Commission Template - Character + Tainted", 1)
-local json = require("json")
+local MyCharacterMod = RegisterMod("Gabriel Character Mod", 1)
+
+-- Character types matching players.xml names
+local moquiType = Isaac.GetPlayerTypeByName("Moqui", false)
+local taintedMoquiType = Isaac.GetPlayerTypeByName("Tainted Moqui", true)
+
+-- Costumes unchanged, referencing original Gabriel assets
+local hairCostume = Isaac.GetCostumeIdByPath("gfx/characters/gabriel_hair.anm2")
+local stolesCostume = Isaac.GetCostumeIdByPath("gfx/characters/gabriel_stoles.anm2")
+
+function MyCharacterMod:GiveCostumesOnInit(player)
+    local pType = player:GetPlayerType()
+    if pType ~= moquiType and pType ~= taintedMoquiType then
+        return
+    end
+    player:AddNullCostume(hairCostume)
+    player:AddNullCostume(stolesCostume)
+end
+MyCharacterMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, MyCharacterMod.GiveCostumesOnInit)
+
 local game = Game()
-local rng = RNG()
+local HOLY_OUTBURST_ID = Isaac.GetItemIdByName("Holy Outburst")
 
---Stat Functions
-local function toTears(fireDelay) --thanks oat for the cool functions for calculating firerate!
-	return 30 / (fireDelay + 1)
-end
-local function fromTears(tears)
-	return math.max((30 / tears) - 1, -0.99)
-end
+-- Cache update for Moqui stats (keep tears, range, shotspeed default)
+function MyCharacterMod:HandleStartingStats(player, flag)
+    local pType = player:GetPlayerType()
 
---Character Functions
----@param name string
----@param isTainted boolean
----@return table
-local function addCharacter(name, isTainted) -- This is the function used to determine the stats of your character, you can simply leave it as you will use it later!
-	local character = { -- these stats are added to Isaac's base stats.
-		NAME = name,
-		ID = Isaac.GetPlayerTypeByName(name, isTainted), -- string, boolean
-	}
-	return character
-end
-mod.One_Character = addCharacter("One", false)
-mod.Two_Character = addCharacter("Two", true)
+    if pType == moquiType then
+        if flag == CacheFlag.CACHE_DAMAGE then
+            player.Damage = 2.5
+        elseif flag == CacheFlag.CACHE_SPEED then
+            player.MoveSpeed = 0.8
+        end
 
-function mod:evalCache(player, cacheFlag) -- this function applies all the stats the character gains/loses on a new run.
-	---@param name string
-	---@param speed number
-	---@param tears number
-	---@param damage number
-	---@param range number
-	---@param shotspeed number
-	---@param luck number
-	---@param tearcolor Color
-	---@param flying boolean
-	---@param tearflag TearFlags
-	local function addStats(name, speed, tears, damage, range, shotspeed, luck, tearcolor, flying, tearflag) -- This is the function used to determine the stats of your character, you can simply leave it as you will use it later!
-		if player:GetPlayerType(name) then
-			if cacheFlag == CacheFlag.CACHE_SPEED then
-				player.MoveSpeed = player.MoveSpeed + speed
-			end
-			if cacheFlag == CacheFlag.CACHE_FIREDELAY then
-				player.MaxFireDelay = math.max(1.0, fromTears(toTears(player.MaxFireDelay) + tears))
-			end
-			if cacheFlag == CacheFlag.CACHE_DAMAGE then
-				player.Damage = player.Damage + damage
-			end
-			if cacheFlag == CacheFlag.CACHE_RANGE then
-				player.TearRange = player.TearRange + range * 40
-			end
-			if cacheFlag == CacheFlag.CACHE_SHOTSPEED then
-				player.ShotSpeed = player.ShotSpeed + shotspeed
-			end
-			if cacheFlag == CacheFlag.CACHE_LUCK then
-				player.Luck = player.Luck + luck
-			end
-			if cacheFlag == CacheFlag.CACHE_TEARCOLOR then
-				player.TearColor = tearcolor
-			end
-			if cacheFlag == CacheFlag.CACHE_FLYING and flying then
-				player.CanFly = true
-			end
-			if cacheFlag == CacheFlag.CACHE_TEARFLAG then
-				player.TearFlags = player.TearFlags | tearflag
-			end
-		end
-	end
-	mod.One_Stats = addStats("One", 0, 0, 0, 0, 0, 0, Color(1, 1, 1, 1.0, 0, 0, 0), false, TearFlags.TEAR_NORMAL)
-	mod.Two_Stats = addStats("Two", 0, 0, 0, 0, 0, 0, Color(1, 1, 1, 1.0, 0, 0, 0), false, TearFlags.TEAR_NORMAL)
-end
-mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE,mod.evalCache)
-
-function mod:playerSpawn(player)
-    if player:GetName() == mod.One_Character.NAME then
-        player:AddNullCostume(Isaac.GetCostumeIdByPath("gfx/characters/One-head.anm2"))
-    end
-    if player:GetName() == mod.Two_Character.NAME then
-        player:AddNullCostume(Isaac.GetCostumeIdByPath("gfx/characters/Two-head.anm2"))
+    elseif pType == taintedMoquiType then
+        if flag == CacheFlag.CACHE_DAMAGE then
+            player.Damage = 3.5
+        elseif flag == CacheFlag.CACHE_SPEED then
+            player.MoveSpeed = 0.9
+        end
     end
 end
-mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, mod.playerSpawn)
+MyCharacterMod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, MyCharacterMod.HandleStartingStats)
 
-----Anything below this is for unlocks
---Saving and Loading Data!
-local persistentData = {
-	unlocks = {
-		One = {
-			MOM = false,
-			MOMSHEART = false,
-			ISAAC = false,
-			BLUEBABY = false,
-			SATAN = false,
-			THELAMB = false,
-			BOSSRUSH = false,
-			HUSH = false,
-			DELIRIUM = false,
-			MEGASATAN = false,
-			MOTHER = false,
-			THEBEAST = false,
-			ULTRAGREED = false,
-			ULTRAGREEDIER = false,
-		},
-		Two = {
-			MOM = false,
-			MOMSHEART = false,
-			ISAAC = false,
-			BLUEBABY = false,
-			SATAN = false,
-			THELAMB = false,
-			BOSSRUSH = false,
-			HUSH = false,
-			DELIRIUM = false,
-			MEGASATAN = false,
-			MOTHER = false,
-			THEBEAST = false,
-			ULTRAGREED = false,
-			ULTRAGREEDIER = false,
-		},
-	}
-}
-
-function mod:GetSaveData()
-	if not mod.persistentData then
-		if mod:HasData() then
-			mod.persistentData = json.decode(mod:LoadData())
-		else
-			mod.persistentData = {}
-		end
-	end
-		return mod.persistentData
+-- Helper to set hearts properly
+local function SetHeartsTo(player, halfHearts)
+    -- Remove all hearts first:
+    player:SetMaxHearts(0)
+    player:AddMaxHearts(halfHearts)
+    player:SetHearts(halfHearts)
 end
 
-if mod:HasData() then
-	persistentData = {
-		unlocks = {
-			One = {
-				MOM = mod:GetSaveData().unlocks.One.MOM,
-				MOMSHEART = mod:GetSaveData().unlocks.One.MOMSHEART,
-				ISAAC = mod:GetSaveData().unlocks.One.ISAAC,
-				BLUEBABY = mod:GetSaveData().unlocks.One.BLUEBABY,
-				SATAN = mod:GetSaveData().unlocks.One.SATAN,
-				THELAMB = mod:GetSaveData().unlocks.One.THELAMB,
-				BOSSRUSH = mod:GetSaveData().unlocks.One.BOSSRUSH,
-				HUSH = mod:GetSaveData().unlocks.One.HUSH,
-				DELIRIUM = mod:GetSaveData().unlocks.One.DELIRIUM,
-				MEGASATAN = mod:GetSaveData().unlocks.One.MEGASATAN,
-				MOTHER = mod:GetSaveData().unlocks.One.MOTHER,
-				THEBEAST = mod:GetSaveData().unlocks.One.THEBEAST,
-				ULTRAGREED = mod:GetSaveData().unlocks.One.ULTRAGREED,
-				ULTRAGREEDIER = mod:GetSaveData().unlocks.One.ULTRAGREEDIER,
-			},
-			Two = {
-				MOM = mod:GetSaveData().unlocks.Two.MOM,
-				MOMSHEART = mod:GetSaveData().unlocks.Two.MOMSHEART,
-				ISAAC = mod:GetSaveData().unlocks.Two.ISAAC,
-				BLUEBABY = mod:GetSaveData().unlocks.Two.BLUEBABY,
-				SATAN = mod:GetSaveData().unlocks.Two.SATAN,
-				THELAMB = mod:GetSaveData().unlocks.Two.THELAMB,
-				BOSSRUSH = mod:GetSaveData().unlocks.Two.BOSSRUSH,
-				HUSH = mod:GetSaveData().unlocks.Two.HUSH,
-				DELIRIUM = mod:GetSaveData().unlocks.Two.DELIRIUM,
-				MEGASATAN = mod:GetSaveData().unlocks.Two.MEGASATAN,
-				MOTHER = mod:GetSaveData().unlocks.Two.MOTHER,
-				THEBEAST = mod:GetSaveData().unlocks.Two.THEBEAST,
-				ULTRAGREED = mod:GetSaveData().unlocks.Two.ULTRAGREED,
-				ULTRAGREEDIER = mod:GetSaveData().unlocks.Two.ULTRAGREEDIER,
-			},
-		}
-	}
+-- Set starting items & stats once on player init
+function MyCharacterMod:OnPlayerInit(player)
+    local pType = player:GetPlayerType()
+
+    if pType == moquiType then
+        -- Add the tick trinket only (items handled by XML)
+        player:AddTrinket(TrinketType.TRINKET_TICK, true)
+
+    elseif pType == taintedMoquiType then
+        -- Give Holy Outburst pocket item for tainted Moqui
+        player:SetPocketActiveItem(HOLY_OUTBURST_ID, ActiveSlot.SLOT_POCKET, true)
+        local pool = game:GetItemPool()
+        pool:RemoveCollectible(HOLY_OUTBURST_ID)
+    end
 end
+MyCharacterMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, MyCharacterMod.OnPlayerInit)
 
-function mod:STOREsavedata()
-	local jsonString = json.encode(persistentData)
-	mod:SaveData(jsonString)
+-- Holy Outburst item usage effect
+function MyCharacterMod:HolyOutburstUse(_, _, player)
+    local spawnPos = player.Position
+    local creep = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_HOLYWATER, 0, spawnPos, Vector.Zero, player):ToEffect()
+    creep.Scale = 2
+    creep:Update()
+    return true
 end
+MyCharacterMod:AddCallback(ModCallbacks.MC_USE_ITEM, MyCharacterMod.HolyOutburstUse, HOLY_OUTBURST_ID)
 
-function mod:LOADsavedata()
-	if mod:HasData() then
-		mod.persistentData = json.decode(mod:LoadData())
-	end
+-- Optional: Holy water trail effect for both characters
+function MyCharacterMod:HandleHolyWaterTrail(player)
+    local pType = player:GetPlayerType()
+    if pType ~= moquiType and pType ~= taintedMoquiType then
+        return
+    end
+    if game:GetFrameCount() % 4 == 0 then
+        local creep = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_HOLYWATER_TRAIL, 0, player.Position, Vector.Zero, player):ToEffect()
+        creep.SpriteScale = Vector(0.5, 0.5)
+        creep:Update()
+    end
 end
-
-function mod:preGameExit()
-	local jsonString = json.encode(persistentData)
-	mod:SaveData(jsonString)
-end
-
-mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, mod.preGameExit)
-
-function mod:OnGameStart(isSave)
-	mod:LOADsavedata()
-end
-mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, mod.OnGameStart)
-
---Debug Console
-function mod.oncmd(_, command, args)
-	if command == "unlocks" and args == mod.One_Character.NAME then
-		print(mod.One_Character.NAME.."'s UNLOCKS ARE AS FOLLOWS")
-		if mod:HasData() then
-			mod:LOADsavedata()
-
-			print("MOM = " .. tostring(mod:GetSaveData().unlocks.One.MOM))
-			print("MOM'S HEART = " .. tostring(mod:GetSaveData().unlocks.One.MOMSHEART))
-			print("ISAAC = " .. tostring(mod:GetSaveData().unlocks.One.ISAAC))
-			print("BLUE BABY = " .. tostring(mod:GetSaveData().unlocks.One.BLUEBABY))
-			print("SATAN = " .. tostring(mod:GetSaveData().unlocks.One.SATAN))
-			print("THE LAMB = " .. tostring(mod:GetSaveData().unlocks.One.THELAMB))
-			print("BOSS RUSH = " .. tostring(mod:GetSaveData().unlocks.One.BOSSRUSH))
-			print("HUSH = " .. tostring(mod:GetSaveData().unlocks.One.HUSH))
-			print("DELIRIUM = " .. tostring(mod:GetSaveData().unlocks.One.DELIRIUM))
-			print("MEGA SATAN = " .. tostring(mod:GetSaveData().unlocks.One.MEGASATAN))
-			print("MOTHER = " .. tostring(mod:GetSaveData().unlocks.One.MOTHER))
-			print("THE BEAST = " .. tostring(mod:GetSaveData().unlocks.One.THEBEAST))
-			print("ULTRA GREED = " .. tostring(mod:GetSaveData().unlocks.One.ULTRAGREED))
-			print("ULTRA GREEDIER = " .. tostring(mod:GetSaveData().unlocks.One.ULTRAGREEDIER))
-		end
-	end
-	if command == "unlocks" and args == mod.Two_Character.NAME then
-		print(mod.Two_Character.NAME.."'s UNLOCKS ARE AS FOLLOWS")
-		if mod:HasData() then
-			mod:LOADsavedata()
-
-			print("MOM = " .. tostring(mod:GetSaveData().unlocks.Two.MOM))
-			print("MOM'S HEART = " .. tostring(mod:GetSaveData().unlocks.Two.MOMSHEART))
-			print("ISAAC = " .. tostring(mod:GetSaveData().unlocks.Two.ISAAC))
-			print("BLUE BABY = " .. tostring(mod:GetSaveData().unlocks.Two.BLUEBABY))
-			print("SATAN = " .. tostring(mod:GetSaveData().unlocks.Two.SATAN))
-			print("THE LAMB = " .. tostring(mod:GetSaveData().unlocks.Two.THELAMB))
-			print("BOSS RUSH = " .. tostring(mod:GetSaveData().unlocks.Two.BOSSRUSH))
-			print("HUSH = " .. tostring(mod:GetSaveData().unlocks.Two.HUSH))
-			print("DELIRIUM = " .. tostring(mod:GetSaveData().unlocks.Two.DELIRIUM))
-			print("MEGA SATAN = " .. tostring(mod:GetSaveData().unlocks.Two.MEGASATAN))
-			print("MOTHER = " .. tostring(mod:GetSaveData().unlocks.Two.MOTHER))
-			print("THE BEAST = " .. tostring(mod:GetSaveData().unlocks.Two.THEBEAST))
-			print("ULTRA GREED = " .. tostring(mod:GetSaveData().unlocks.Two.ULTRAGREED))
-			print("ULTRA GREEDIER = " .. tostring(mod:GetSaveData().unlocks.Two.ULTRAGREEDIER))
-		end
-	end
-	--auto unlock all
-	if command == "unlocks" and args == mod.One_Character.NAME .. " unlockall" then
-		print(mod.One_Character.NAME.."'s UNLOCKS ARE ALL UNLOCKED")
-		if mod:HasData() then
-			persistentData.unlocks.One.MOM = true
-			persistentData.unlocks.One.MOMSHEART = true
-			persistentData.unlocks.One.ISAAC = true
-			persistentData.unlocks.One.BLUEBABY = true
-			persistentData.unlocks.One.SATAN = true
-			persistentData.unlocks.One.THELAMB = true
-			persistentData.unlocks.One.BOSSRUSH = true
-			persistentData.unlocks.One.HUSH = true
-			persistentData.unlocks.One.DELIRIUM = true
-			persistentData.unlocks.One.MEGASATAN = true
-			persistentData.unlocks.One.MOTHER = true
-			persistentData.unlocks.One.THEBEAST = true
-			persistentData.unlocks.One.ULTRAGREED = true
-			persistentData.unlocks.One.ULTRAGREEDIER = true
-
-			mod:STOREsavedata()
-		end
-	end
-	if command == "unlocks" and args == mod.Two_Character.NAME .. " unlockall" then
-		print(mod.Two_Character.NAME.."'s UNLOCKS ARE ALL UNLOCKED")
-		if mod:HasData() then
-			persistentData.unlocks.Two.MOM = true
-			persistentData.unlocks.Two.MOMSHEART = true
-			persistentData.unlocks.Two.ISAAC = true
-			persistentData.unlocks.Two.BLUEBABY = true
-			persistentData.unlocks.Two.SATAN = true
-			persistentData.unlocks.Two.THELAMB = true
-			persistentData.unlocks.Two.BOSSRUSH = true
-			persistentData.unlocks.Two.HUSH = true
-			persistentData.unlocks.Two.DELIRIUM = true
-			persistentData.unlocks.Two.MEGASATAN = true
-			persistentData.unlocks.Two.MOTHER = true
-			persistentData.unlocks.Two.THEBEAST = true
-			persistentData.unlocks.Two.ULTRAGREED = true
-			persistentData.unlocks.Two.ULTRAGREEDIER = true
-
-			mod:STOREsavedata()
-		end
-	end
-	--auto relock all
-	if command == "unlocks" and args == mod.One_Character.NAME .. " lockall" then
-		print(mod.One_Character.NAME.."'s UNLOCKS ARE ALL LOCKED")
-		if mod:HasData() then
-			persistentData.unlocks.One.MOM = false persistentData.unlocks.One.MOMSHEART = false
-			persistentData.unlocks.One.ISAAC = false persistentData.unlocks.One.BLUEBABY = false
-			persistentData.unlocks.One.SATAN = false persistentData.unlocks.One.THELAMB = false
-			persistentData.unlocks.One.BOSSRUSH = false persistentData.unlocks.One.HUSH = false
-			persistentData.unlocks.One.DELIRIUM = false persistentData.unlocks.One.MEGASATAN = false
-			persistentData.unlocks.One.MOTHER = false persistentData.unlocks.One.THEBEAST = false
-			persistentData.unlocks.One.ULTRAGREED = false persistentData.unlocks.One.ULTRAGREEDIER = false
-
-			mod:STOREsavedata()
-		end
-	end
-	if command == "unlocks" and args == mod.Two_Character.NAME .. " lockall" then
-		print(mod.Two_Character.NAME.."'s UNLOCKS ARE ALL LOCKED")
-		if mod:HasData() then
-			persistentData.unlocks.Two.MOM = false persistentData.unlocks.Two.MOMSHEART = false
-			persistentData.unlocks.Two.ISAAC = false persistentData.unlocks.Two.BLUEBABY = false
-			persistentData.unlocks.Two.SATAN = false persistentData.unlocks.Two.THELAMB = false
-			persistentData.unlocks.Two.BOSSRUSH = false persistentData.unlocks.Two.HUSH = false
-			persistentData.unlocks.Two.DELIRIUM = false persistentData.unlocks.Two.MEGASATAN = false
-			persistentData.unlocks.Two.MOTHER = false persistentData.unlocks.Two.THEBEAST = false
-			persistentData.unlocks.Two.ULTRAGREED = false persistentData.unlocks.Two.ULTRAGREEDIER = false
-
-			mod:STOREsavedata()
-		end
-	end
-end
-mod:AddCallback(ModCallbacks.MC_EXECUTE_CMD, mod.oncmd)
+MyCharacterMod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, MyCharacterMod.HandleHolyWaterTrail)
